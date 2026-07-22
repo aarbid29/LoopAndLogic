d0ada8ab62ca77a560ab48276a13a82bf4ce15ee
Aarbid <aarbid.b@gmail.com>
2026-07-22 18:46:08 +0545

    Time: 52 ms (44.78%) | Memory: 19.3 MB (67.22%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.py
+++ 377-combination-sum-iv/combination-sum-iv.py
@@ -1,6 +1,8 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        @lru_cache(None)
+
+        memo = {}
+
         def dfs(summ):
             if summ == target:
                 return 1
@@ -8,11 +10,15 @@ class Solution:
             if summ > target:
                 return 0
 
+            if summ in memo:
+                return memo[summ]
+
             count = 0
 
             for num in nums:
                 count += dfs(summ + num)
 
+            memo[summ] = count
             return count
 
         return dfs(0)
