24e5322e2f828a58664348dc5bf1399b68d4100d
Aarbid <aarbid.b@gmail.com>
2026-07-23 10:56:22 +0545

    Time: 0 ms (100.00%) | Memory: 19.4 MB (41.35%) - LeetSync

---
+++ 62-unique-paths/unique-paths.py
@@ -0,0 +1,23 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+
+        # create dp matrix
+        dp = [[0] * n for _ in range(m)]
+
+        # base case
+        dp[m-1][n-1] = 1
+
+        # traverse from bottom-right to top-left
+        for r in range(m - 1, -1, -1):
+            for c in range(n - 1, -1, -1):
+
+                # skip destination cell
+                if r == m - 1 and c == n - 1:
+                    continue
+
+                down = dp[r + 1][c] if r + 1 < m else 0
+                right = dp[r][c + 1] if c + 1 < n else 0
+
+                dp[r][c] = down + right
+
+        return dp[0][0]
