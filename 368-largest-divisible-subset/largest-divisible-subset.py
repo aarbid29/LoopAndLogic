c986ac5f9e8068a988e5384764b5459f87e15b3c
Aarbid <aarbid.b@gmail.com>
2026-07-27 20:24:14 +0545

    Time: 190 ms (25.56%) | Memory: 19.6 MB (31.65%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.py
+++ 368-largest-divisible-subset/largest-divisible-subset.py
@@ -1,21 +1,17 @@
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
-        nums.sort()
 
+        nums.sort()
+        dp = [ [] for _ in range(len(nums)+1)]
         n = len(nums)
-        dp = [[nums[i]] for i in range(n)]
 
         for i in range(n-1, -1, -1):
+            res = [nums[i]]
             for j in range(i+1, n):
-                if nums[j] % nums[i] == 0:
-                    candidate = [nums[i]] + dp[j]
+                if nums[j]% nums[i] == 0 :
+                    tmp = [nums[i]] + dp[j]
+                    if len(tmp)> len(res):
+                        res = tmp
+            dp[i] = res
+        return max(dp,key = len)
 
-                    if len(candidate) > len(dp[i]):
-                        dp[i] = candidate
-
-        res = []
-        for subset in dp:
-            if len(subset) > len(res):
-                res = subset
-
-        return res
