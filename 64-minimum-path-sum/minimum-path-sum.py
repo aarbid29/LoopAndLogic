e96485aecbdf06e8717325b3d956460228c7a9c0
Aarbid <aarbid.b@gmail.com>
2026-07-30 10:14:21 +0545

    Time: 19 ms (26.25%) | Memory: 22 MB (19.35%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,20 +1,37 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
+        row = len(grid)
+        col = len(grid[0])
+        # def dfs(r,c):
+        #     if r== len(grid)-1 and c==len(grid[0])-1:
+        #         return grid[r][c]
 
-        dp = [[0] * n for _ in range(m)]
+        #     if r >=row or c >= col:
+        #         return float("inf")
+        #     right = grid[r][c] +dfs(r,c+1)
+        #     down = grid[r][c]+ dfs(r+1,c)
 
-        for r in range(m - 1, -1, -1):
-            for c in range(n - 1, -1, -1):
+        #     return min(right,down)
 
-                if r == m - 1 and c == n - 1:
-                    dp[r][c] = grid[r][c]
+        # return dfs(0,0)
+        dp = [[float("inf") for _ in range(col + 1)] for _ in range(row + 1)]
+        dp[row-1][col-1] = grid[row-1][col-1]
+
+        for r in range(row-1,-1,-1):
+            for c in range(col-1,-1,-1):
+                if (r, c) == (row - 1, col - 1):
+                    dp[row - 1][col - 1] = grid[row - 1][col - 1]
                     continue
+                right = grid[r][c] + dp[r][c+1]
+                down = grid[r][c]+ dp[r+1][c]
 
-                down = dp[r + 1][c] if r + 1 < m else float('inf')
-                right = dp[r][c + 1] if c + 1 < n else float('inf')
-
-                dp[r][c] = grid[r][c] + min(down, right)
-
+                dp[r][c] = min(right,down)
+        
         return dp[0][0]
+
+        
+
+
+
+
+        
