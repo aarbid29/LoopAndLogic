5ac3e6c873c1230618990f442229a5a93b02c876
Aarbid <aarbid.b@gmail.com>
2026-07-31 21:04:26 +0545

    Time: 0 ms (100.00%) | Memory: 19.5 MB (17.39%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -0,0 +1,8 @@
+from functools import lru_cache
+
+class Solution:
+    @lru_cache(None)
+    def tribonacci(self, n: int) -> int:
+        if n <= 2:
+            return 1 if n != 0 else 0
+        return self.tribonacci(n - 1) + self.tribonacci(n - 2) + self.tribonacci(n - 3)
