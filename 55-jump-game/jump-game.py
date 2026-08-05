3cb56fa6cd7d32ce6b48df1cbe575dd409186749
Aarbid <aarbid.b@gmail.com>
2026-08-05 16:01:42 +0545

    Time: 15 ms (78.43%) | Memory: 20.2 MB (84.75%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,17 +1,14 @@
-from functools import lru_cache
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        @lru_cache(None)
-        def dfs(step):
-            if step == len(nums) - 1:
-                return True
-            if step >= len(nums):
-                return False
+        reached = [False] * (len(nums) + 1)
+        reached[len(nums)-1] = True
+        prev_true = len(nums)-1
 
-            for i in range(1, nums[step] + 1):
-                if dfs(step + i):
-                    return True
-            
-            return False
+        for i in range(len(nums)-2, -1, -1):
+            if i + nums[i] >= prev_true:
+                prev_true = i
+                reached[i] = True
+            else:
+                reached[i] = False
 
-        return dfs(0)
+        return reached[0]
