b5cc4e36e361b1c5253ce4592b54a6936c096880
Aarbid <aarbid.b@gmail.com>
2026-08-07 08:45:28 +0545

    Time: 3 ms (30.15%) | Memory: 21.2 MB (89.11%) - LeetSync

---
+++ 57-insert-interval/insert-interval.py
@@ -0,0 +1,40 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        intervals.append(newInterval)
+        intervals.sort()
+
+        new_beg = intervals[0][0]
+        new_end = intervals[0][1]
+        res = []
+        for start, end in intervals[1:]:
+            if start > new_end:
+                res.append([new_beg, new_end])
+                new_beg = start
+                new_end = end
+            elif end > new_end:
+                new_end = end
+
+        res.append([new_beg, new_end])
+        return res
+
+
+
+
+
+           
+            
+
+
+
+
+
+
+
+            
+
+
+            
+
+
+
+        
