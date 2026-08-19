b9d6d24dc12d04c6c3a6a9cf5cb52745382bc49d
Aarbid <aarbid.b@gmail.com>
2026-08-19 15:41:24 +0545

    Time: 338 ms (60.39%) | Memory: 21.6 MB (18.55%) - LeetSync

--- 744-network-delay-time/network-delay-time.py
+++ 744-network-delay-time/network-delay-time.py
@@ -1,23 +1,27 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
 
-        adj = defaultdict(list)
-        distance = [float("inf") for _ in range(n + 1)]
-        distance[k] = 0
-
-        for u, v, w in times:
-            adj[u].append((v, w))
-
         heap = []
         heapq.heappush(heap, (0, k))
 
-        while heap:
-            weight, node = heapq.heappop(heap)
-            for neigh, val in adj[node]:
-                if distance[node] + val < distance[neigh]:
-                    distance[neigh] = distance[node] + val
-                    heapq.heappush(heap, (distance[neigh], neigh))
+        adj = defaultdict(list)
 
-        if float("inf") in distance[1:]:
+        for u, v, weight in times:
+            adj[u].append((weight, v))
+
+        time = [float("inf")] * (n + 1)
+        time[k] = 0
+
+        while heap:
+            val, node = heapq.heappop(heap)
+
+            for weight, neigh in adj[node]:
+                
+                if val + weight < time[neigh]:
+                    time[neigh] = val + weight
+                    heapq.heappush(heap, (time[neigh], neigh))
+
+        if float("inf") in time[1:]:
             return -1
-        return max(distance[1:])
+
+        return max(time[1:])
