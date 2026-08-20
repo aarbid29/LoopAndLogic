7331726ec58c7d8dd0036c16cb59d84b6c263274
Aarbid <aarbid.b@gmail.com>
2026-08-20 11:58:26 +0545

    Time: 25 ms (95.77%) | Memory: 32.6 MB (85.38%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.py
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.py
@@ -1,70 +1,54 @@
 class Solution:
     def makeConnected(self, n: int, connections: List[List[int]]) -> int:
-
-        if len(connections) < n - 1:
-            return -1
-        #adj list crecate
         class DSU:
-            def __init__(self, n):
+            def __init__(self,n):
                 self.parent = list(range(n))
-                self.rank = [1] * n
+                self.rank = [1]*n
 
-            def find(self, x):
-                if self.parent[x] != x:
+            def find(self,x):
+                if self.parent[x]!=x:
                     self.parent[x] = self.find(self.parent[x])
                 return self.parent[x]
 
-            def union(self, x, y):
+            def union(self, x,y):
                 px = self.find(x)
-                py = self.find(y)
+                py = self. find(y)
 
                 if px == py:
                     return False
-
-                if self.rank[px] > self.rank[py]:
+                if self.rank[px]>self.rank[py]:
                     self.parent[py] = px
-                elif self.rank[py] > self.rank[px]:
+                elif self.rank[py]>self.rank[px]:
                     self.parent[px] = py
                 else:
                     self.parent[py] = px
-                    self.rank[px] += 1
-
+                    self.rank[px]+=1
                 return True
 
         dsu = DSU(n)
-        count =0 
+        visited = set()
+        used = 0 
+        extra = 0
 
-        for c1,c2 in connections:
-            if dsu.union(c1,c2):
-                continue
+        for u ,v in connections:
+            if dsu.union(u,v):
+                used+=1
             else:
-                count+=1
+                extra +=1
+        needed = 0
 
-        
-        parents = set()
-
-        for i in range(n):
-            parents.add(dsu.find(i))
-
-        components = len(parents)
-
-        if count>= components-1:
-            return components-1
+        needed = n-used-1
+        if extra >=needed:
+            return needed
         else:
             return -1
-
             
-                          
-
-        
-
-    
-
-
-
-
-
-
 
 
         
+
+
+
+
+
+
