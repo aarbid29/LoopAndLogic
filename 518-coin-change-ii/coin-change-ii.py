d9802af85caf55a9075f4820c1531eb0f4b04fdb
Aarbid <aarbid.b@gmail.com>
2026-08-24 14:25:06 +0545

    Time: 1574 ms (8.13%) | Memory: 520.4 MB (5.03%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,10 +1,18 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        dp = [0] * (amount + 1)
-        dp[0] = 1
+        @lru_cache(None)
+        def dfs(i,summ):
+            if summ == amount:
+                return 1 
+            if summ>amount:
+                return 0
+            if i >=len(coins):
+                return 0
 
-        for coin in coins:
-            for s in range(coin, amount + 1):
-                dp[s] += dp[s - coin]
+            add =  dfs(i,summ+coins[i]) + dfs(i+1,summ)
+            return add
 
-        return dp[amount]
+        return dfs(0,0)
+
+    
+
