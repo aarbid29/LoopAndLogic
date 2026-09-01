ed0616efc27328b2b4d0755686604516051d9495
Aarbid <aarbid.b@gmail.com>
2026-09-01 14:32:33 +0545

    Time: 11 ms (66.51%) | Memory: 19.9 MB (10.31%) - LeetSync

---
+++ 51-n-queens/n-queens.py
@@ -0,0 +1,57 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        
+        matrix = [["."]*n for _ in range(n)]
+        res = []
+        col_visited= set()
+        diag1_visited = set() #for /
+        diag2_visited= set() # for \
+
+
+        def backtrack(r):
+
+
+            for col in range(n):
+                r,c = r,col
+
+                if c in col_visited:
+                    continue
+                #diag check:
+                if (r-c) in diag2_visited:
+                    continue
+                if(r+c) in diag1_visited:
+                    continue
+            
+                matrix[r][c]= 'Q'
+                col_visited.add(c)
+
+                diag1_visited.add(r+c)
+                diag2_visited.add(r-c)
+
+                if r == n - 1:
+                    res.append(["".join(row) for row in matrix])
+                else:
+                    backtrack(r + 1)
+
+
+                diag1_visited.remove(r+c)
+                diag2_visited.remove(r-c)
+                col_visited.remove(c)
+                matrix[r][c]= "."
+        
+        backtrack(0)
+        return res
+            
+            
+
+
+
+
+
+
+
+
+
+            
+
+
