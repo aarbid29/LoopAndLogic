446f7d7773fb67efb78ae1be20bd7d3d1728fddd
Aarbid <aarbid.b@gmail.com>
2026-09-05 16:07:00 +0545

    Time: 700 ms (5.32%) | Memory: 32.3 MB (24.21%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.py
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.py
@@ -1,41 +1,44 @@
 class Solution:
     def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
-        n =  len(bloomDay)
-        if n < m * k:
+        if m * k > len(bloomDay):
             return -1
-        l = 0 
-        maxx = max(bloomDay)
-        r = maxx
-
-        while l<r :
-            midd = (l+r)//2
-            time = 0 
-            i = 0 
+        l = 1 
+        r = max(bloomDay)
+        while l < r :
+            day = (l+r)//2
+            cnt = 0
+            i = 0
             boq = 0 
+            while i < len(bloomDay):
+                if bloomDay[i] <= day:
+                    cnt += 1
+                    if cnt == k:
+                        boq += 1
+                        cnt = 0
+                    i += 1
+                else:
+                    cnt = 0
+                    i += 1
 
-            #can m boq be made with midd-> day?
-            while i<n:
-                flower = bloomDay[i]
-                adj = 0 
-
-                while i < n and bloomDay[i]<=midd:
-                    adj+=1
-                    i+=1
-                    if adj == k:
-                        boq+=1
-                        break
-                if i < n and bloomDay[i] > midd:
-                    i+=1
-                
-            if boq < m:
-                l = midd + 1
+            if boq >=m:
+                r= day
             else:
-                r = midd 
-        
+                l = day +1 
         return l
-
+                
+                
+                
                 
 
+
+
+
+
+
+
+
+
+            
                 
 
 
@@ -44,8 +47,13 @@ class Solution:
 
 
 
-        
 
-        
+
+
+
+
+
+
+
 
         
