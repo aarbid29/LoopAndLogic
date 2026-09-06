0e5783fcf494ecf6623d2db6600144d58c549d1a
Aarbid <aarbid.b@gmail.com>
2026-09-06 09:09:13 +0545

    Time: 723 ms (19.35%) | Memory: 255.9 MB (5.10%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -1,22 +1,29 @@
-from functools import cache
-
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
-        n = len(s)
-        m = len(t)
-
-        @cache
-        def dfs(i, j):
-            if j == m:
-                return 1
-            if i == n:
+        m= len(s)
+        n = len(t)
+        count = 0 
+        @lru_cache(None)
+        def dfs(i,j):
+            #i,j means im currently checking those indices on s and t 
+            if i >=m and j>=n:
+                return 1 
+                
+            if i>=m and j<n:
                 return 0
 
-            if s[i] == t[j]:
-                take = dfs(i + 1, j + 1)
-                no_take = dfs(i + 1, j)
-                return take + no_take
-            else:
-                return dfs(i + 1, j)
+            if j == n:
+                return 1 
 
-        return dfs(0, 0)
+            #if matched , we have two possibilites , take or no_take curr i
+            if s[i]==t[j]:
+                return dfs(i+1,j) + dfs(i+1,j+1)
+            else:
+                return dfs(i+1,j)
+            
+
+        
+        return dfs(0,0)
+            
+
+        
