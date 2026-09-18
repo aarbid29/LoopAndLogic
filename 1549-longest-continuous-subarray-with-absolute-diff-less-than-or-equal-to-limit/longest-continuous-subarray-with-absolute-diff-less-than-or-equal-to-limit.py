2e10b9944657bb240460e7c41150a07a22b87f08
Aarbid <aarbid.b@gmail.com>
2026-09-18 17:20:25 +0545

    Time: 171 ms (92.14%) | Memory: 30.1 MB (80.21%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,30 +1,40 @@
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
-        l = 0
-        max_dq = deque()
-        min_dq = deque()
-        maxx = 0
+        count = 0 
+        #i can traverse from 8 to 7 , but how to start subarray and check for it from 2 to 7 , then 4 to 7 then 7 itself
+        max_dq = deque()#max ele in front
+        min_dq = deque()#min ele in front
+        maxx = 0 # for max length
+        l =0 
 
         for r in range(len(nums)):
             curr = nums[r]
-
-            while max_dq and max_dq[-1] < curr:
+            while max_dq and curr > max_dq[-1]:
                 max_dq.pop()
             max_dq.append(curr)
 
-            while min_dq and min_dq[-1] > curr:
-                min_dq.pop()
+            while min_dq and curr < min_dq[-1]:
+                min_dq.pop() #as min_dp[-1]has alr been processed as itself as the min
             min_dq.append(curr)
-
+            
             while max_dq[0] - min_dq[0] > limit:
-                if nums[l] == max_dq[0]:
+                left = nums[l]
+
+                if left == max_dq[0]:
                     max_dq.popleft()
-
-                if nums[l] == min_dq[0]:
+                
+                if left == min_dq[0]:
                     min_dq.popleft()
-
-                l += 1
-
-            maxx = max(maxx, r - l + 1)
-
+                l+=1
+            
+            maxx = max(maxx,r-l+1)
         return maxx
+
+
+
+
+
+
+
+
+        
