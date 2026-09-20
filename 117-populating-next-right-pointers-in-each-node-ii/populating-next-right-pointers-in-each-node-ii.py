a06b66436e458c8558214dfff0c790fa1812f25c
Aarbid <aarbid.b@gmail.com>
2026-09-20 15:12:28 +0545

    Time: 53 ms (47.29%) | Memory: 20.3 MB (64.65%) - LeetSync

--- 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.py
+++ 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.py
@@ -1,23 +1,23 @@
 class Solution:
     def connect(self, root: 'Node') -> 'Node':
-        if not root:
-            return None
-        dq = deque([root])
 
-        while dq:
-            size = len(dq)
-            for i in range(size):
-                node= dq.popleft()
-                second_node = dq[0] if dq else None
-                if i == size-1:
-                    node.next = None
-                else:
-                    node.next = second_node
-    
-                if node.left:
-                    dq.append(node.left)
-                if node.right:
-                    dq.append(node.right)
+        curr = root
 
-        
+        while curr : 
+            dummy = Node(0)
+            tail = dummy
+
+
+            while curr:
+                if curr.left:
+                        tail.next = curr.left
+                        tail = tail.next
+                if curr.right:
+                        tail.next = curr.right
+                        tail = tail.next
+                    
+                curr = curr.next
+                
+            curr = dummy.next
         return root
+       
