f84f2623ff4b7761abf2adf5cc60092766a0265f
Aarbid <aarbid.b@gmail.com>
2026-09-22 09:21:11 +0545

    Time: 45 ms (94.40%) | Memory: 19.7 MB (18.02%) - LeetSync

--- 696-count-binary-substrings/count-binary-substrings.py
+++ 696-count-binary-substrings/count-binary-substrings.py
@@ -1,31 +1,26 @@
 class Solution:
     def countBinarySubstrings(self, s: str) -> int:
-        track = 0
-        def calc(z,o):
-            nonlocal track
-            prevz = s[z]
-            prevo = s[o]
-            #expand from , need of index
-            while z>=0 and o<len(s) and s[z]==prevz and s[o]==prevo:
-                track+=1
-                prevz = s[z]
-                prev0 = s[o]
-                z-=1
-                o+=1
-            return
+
+        prev = 0 
+        curr = 0 
+        ans = 0
+
+        for i in range(len(s)):
+
+            if s[i] == s[i-1]:
+                curr+=1
+            else:
+                ans+= min(curr,prev)
+                prev = curr
+                curr = 1 
+        
+        ans += min(curr,prev)
+        return ans
+        
+            
+
+
 
 
 
         
-        for i in range(len(s)):
-            if  i == len(s)-1:
-                continue
-            curr  = s[i]
-            nextt = s[i+1]
-            if (curr == "0"and nextt=="1") or (curr == "1"and nextt=="0"):
-                # track+=1
-                calc(i,i+1)
-        return track
-            
-
-           
