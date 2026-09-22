219e4b7b87047204cad9446871cf07169cd4fb1f
Aarbid <aarbid.b@gmail.com>
2026-09-22 09:21:46 +0545

    Time: 51 ms (83.56%) | Memory: 19.6 MB (64.96%) - LeetSync

--- 696-count-binary-substrings/count-binary-substrings.py
+++ 696-count-binary-substrings/count-binary-substrings.py
@@ -2,10 +2,10 @@ class Solution:
     def countBinarySubstrings(self, s: str) -> int:
 
         prev = 0 
-        curr = 0 
+        curr = 1 
         ans = 0
 
-        for i in range(len(s)):
+        for i in range(1,len(s)):
 
             if s[i] == s[i-1]:
                 curr+=1
